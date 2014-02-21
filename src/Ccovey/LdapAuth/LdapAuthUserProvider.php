1c3b130b1349a041572da96c77b94c84f5acf595
Tortue Torche <tortuetorche@spam.me>
2014-02-21 15:48:26 +0100

    Switch to tabs indentation to keep the original package convention.

--- src/Ccovey/LdapAuth/LdapAuthUserProvider.php
+++ src/Ccovey/LdapAuth/LdapAuthUserProvider.php
@@ -11,218 +11,218 @@ use Illuminate\Auth;
  */
 class LdapAuthUserProvider implements Auth\UserProviderInterface
 {
-    /**
-     * Active Directory Object
-     * 
-     * @var adLDAP\adLDAP
-     */
-    protected $ad;
-    
-    /**
-     *
-     * @var type string
-     */
-    protected $model;
-    
-    /**
-     * DI in adLDAP object for use throughout
-     * 
-     * @param adLDAP\adLDAP $conn
-     */
-    public function __construct(adLDAP\adLDAP $conn, $config, $model = null)
-    {
-        $this->ad = $conn;
+	/**
+	 * Active Directory Object
+	 *
+	 * @var adLDAP\adLDAP
+	 */
+	protected $ad;
 
-        $this->config = $config;
-        
-        $this->model = $model;
-    }
+	/**
+	 *
+	 * @var type string
+	 */
+	protected $model;
 
-    /**
-     * Retrieve a user by their unique identifier.
-     *
-     * @param  mixed  $identifier
-     * @param  mixed  $ldapIdentifier; default to null
-     * @return Illuminate\Auth\GenericUser|null
-     */
-    public function retrieveByID($identifier, $ldapIdentifier = null)
-    {
-        if( is_null($ldapIdentifier) ) {
-            $ldapIdentifier = $identifier;
-        }
-        $ldapUserInfo = null;
+	/**
+	 * DI in adLDAP object for use throughout
+	 *
+	 * @param adLDAP\adLDAP $conn
+	 */
+	public function __construct(adLDAP\adLDAP $conn, $config, $model = null)
+	{
+		$this->ad = $conn;
 
-        $infoCollection = $this->ad->user()->infoCollection($ldapIdentifier, array('*') );
+		$this->config = $config;
 
-        if ( $infoCollection ) {
-            $ldapUserInfo = $this->setInfoArray($infoCollection);
+		$this->model = $model;
+	}
 
-            if ($this->model) {
-                $model = $this->createModel()->newQuery()->where($this->getUsernameField(), $identifier)->first();
+	/**
+	 * Retrieve a user by their unique identifier.
+	 *
+	 * @param  mixed  $identifier
+	 * @param  mixed  $ldapIdentifier; default to null
+	 * @return Illuminate\Auth\GenericUser|null
+	 */
+	public function retrieveByID($identifier, $ldapIdentifier = null)
+	{
+		if( is_null($ldapIdentifier) ) {
+			$ldapIdentifier = $identifier;
+		}
+		$ldapUserInfo = null;
 
-                if ( ! is_null($model) ) {
-                    return $this->addLdapToModel($model, $ldapUserInfo);
-                }
-            }
+		$infoCollection = $this->ad->user()->infoCollection($ldapIdentifier, array('*') );
 
-            return new LdapUser((array) $ldapUserInfo);
-        }
-    }
+		if ( $infoCollection ) {
+			$ldapUserInfo = $this->setInfoArray($infoCollection);
 
-    /**
-     * Retrieve a user by the given credentials.
-     *
-     * @param  array  $credentials
-     * @return Illuminate\Auth\GenericUser|null
-     */
-    public function retrieveByCredentials(array $credentials)
-    {
-        if (! $user = $credentials[$this->getUsernameField()]) {
-            throw new InvalidArgumentException;
-        }
+			if ($this->model) {
+				$model = $this->createModel()->newQuery()->where($this->getUsernameField(), $identifier)->first();
 
-        if ($this->model) {
-            $query = $this->createModel()->newQuery();
+				if ( ! is_null($model) ) {
+					return $this->addLdapToModel($model, $ldapUserInfo);
+				}
+			}
 
-            foreach ($credentials as $key => $value)
-            {
-                if ( ! str_contains($key, 'password')) $query->where($key, $value);
-            }
+			return new LdapUser((array) $ldapUserInfo);
+		}
+	}
 
-            $model = $query->first();
-            if($model) {
-                return $this->retrieveByID($model->getKey(), $model->{$this->getUsernameField()});
-            }
-        }
+	/**
+	 * Retrieve a user by the given credentials.
+	 *
+	 * @param  array  $credentials
+	 * @return Illuminate\Auth\GenericUser|null
+	 */
+	public function retrieveByCredentials(array $credentials)
+	{
+		if (! $user = $credentials[$this->getUsernameField()]) {
+			throw new InvalidArgumentException;
+		}
 
-        return $this->retrieveByID($user);
-    }
+		if ($this->model) {
+			$query = $this->createModel()->newQuery();
 
-    /**
-     * Validate a user against the given credentials.
-     *
-     * @param  Illuminate\Auth\UserInterface  $user
-     * @param  array  $credentials
-     * @return bool
-     */
-    public function validateCredentials(Auth\UserInterface $user, array $credentials)
-    {
-        return $this->ad->authenticate($credentials['username'], $credentials['password']);
-    }
-    
-    /**
-     * Build the array sent to GenericUser for use in Auth::user()
-     * 
-     * @param adLDAP\adLDAP $infoCollection
-     * @return array $info
-     */
-    protected function setInfoArray($infoCollection)
-    {
-        /*
-        * in app/auth.php set the fields array with each value
-        * as a field you want from active directory
-        * If you have 'user' => 'samaccountname' it will set the $info['user'] = $infoCollection->samaccountname
-        * refer to the adLDAP docs for which fields are available.
-        */
-        if ( ! empty($this->config['fields'])) {
-            foreach ($this->config['fields'] as $k => $field) {
-                if ($k == 'groups') {
-                    $info[$k] = $this->getAllGroups($infoCollection->memberof);
-                }elseif ($k == 'primarygroup') {
-                    $info[$k] = $this->getPrimaryGroup($infoCollection->distinguishedname);
-                }else{    
-                    $info[$k] = $infoCollection->$field;
-                }
-            }
-        }else{
-            //if no fields array present default to username and displayName
-            $info['username'] = $infoCollection->samaccountname;
-            $info['displayname'] = $infoCollection->displayName;
-            $info['primarygroup'] = $this->getPrimaryGroup($infoCollection->distinguishedname);
-            $info['groups'] = $this->getAllGroups($infoCollection->memberof);
-        }
-        
-        /*
-        * I needed a user list to populate a dropdown
-        * Set userlist to true in app/config/auth.php and set a group in app/config/auth.php as well
-        * The table is the OU in Active directory you need a list of.
-        */
-        if ( ! empty($this->config['userList'])) {
-            $info['userlist'] = $this->ad->folder()->listing(array($this->config['group']));
-        }
+			foreach ($credentials as $key => $value)
+			{
+				if ( ! str_contains($key, 'password')) $query->where($key, $value);
+			}
 
-        return $info;
-    }
+			$model = $query->first();
+			if($model) {
+				return $this->retrieveByID($model->getKey(), $model->{$this->getUsernameField()});
+			}
+		}
 
-    /**
-     * 
-     * @return Illuminate\Auth\UserInterface
-     */
-    public function createModel()
-    {   
-        $model = '\\' . ltrim($this->model, '\\');
-        
-        return new $model;
-    }
+		return $this->retrieveByID($user);
+	}
 
-    /**
-     * Add Ldap fields to current user model.
-     * 
-     * @param Illuminate\Auth\UserInterface $model
-     * @param adLDAP\collection\adLDAPCollection $ldap
-     * @return Illuminate\Auth\UserInterface
-     */
-    protected function addLdapToModel($model, $ldap)
-    {
-        $combined = $ldap + $model->getAttributes();
+	/**
+	 * Validate a user against the given credentials.
+	 *
+	 * @param  Illuminate\Auth\UserInterface  $user
+	 * @param  array  $credentials
+	 * @return bool
+	 */
+	public function validateCredentials(Auth\UserInterface $user, array $credentials)
+	{
+		return $this->ad->authenticate($credentials['username'], $credentials['password']);
+	}
 
-        return $model->fill($combined);
-    }
+	/**
+	 * Build the array sent to GenericUser for use in Auth::user()
+	 *
+	 * @param adLDAP\adLDAP $infoCollection
+	 * @return array $info
+	 */
+	protected function setInfoArray($infoCollection)
+	{
+		/*
+		* in app/auth.php set the fields array with each value
+		* as a field you want from active directory
+		* If you have 'user' => 'samaccountname' it will set the $info['user'] = $infoCollection->samaccountname
+		* refer to the adLDAP docs for which fields are available.
+		*/
+		if ( ! empty($this->config['fields'])) {
+			foreach ($this->config['fields'] as $k => $field) {
+				if ($k == 'groups') {
+					$info[$k] = $this->getAllGroups($infoCollection->memberof);
+				}elseif ($k == 'primarygroup') {
+					$info[$k] = $this->getPrimaryGroup($infoCollection->distinguishedname);
+				}else{
+					$info[$k] = $infoCollection->$field;
+				}
+			}
+		}else{
+			//if no fields array present default to username and displayName
+			$info['username'] = $infoCollection->samaccountname;
+			$info['displayname'] = $infoCollection->displayName;
+			$info['primarygroup'] = $this->getPrimaryGroup($infoCollection->distinguishedname);
+			$info['groups'] = $this->getAllGroups($infoCollection->memberof);
+		}
 
-    /**
-     * Return Primary Group Listing
-     * @param  array $groupList 
-     * @return string
-     */
-    protected function getPrimaryGroup($groupList)
-    {
-        $groups = explode(',', $groupList);
+		/*
+		* I needed a user list to populate a dropdown
+		* Set userlist to true in app/config/auth.php and set a group in app/config/auth.php as well
+		* The table is the OU in Active directory you need a list of.
+		*/
+		if ( ! empty($this->config['userList'])) {
+			$info['userlist'] = $this->ad->folder()->listing(array($this->config['group']));
+		}
 
-        return substr($groups[1], '3');
-    }
+		return $info;
+	}
 
-    /**
-     * Return list of groups (except domain and suffix)
-     * @param  array $groups 
-     * @return array
-     */
-    protected function getAllGroups($groups) 
-    {
-        $grps = '';
-        if ( ! is_null($groups) ) {
-            if (!is_array($groups)) {
-                $groups = explode(',', $groups);
-            }
-            foreach ($groups as $k => $group) {
-                $splitGroups = explode(',', $group);
-                foreach ($splitGroups as $splitGroup) {
-                    if (substr($splitGroup,0, 3) !== 'DC=') {
-                        $grps[substr($splitGroup, '3')] = substr($splitGroup, '3');
-                    }
-                }
-            }
-        }
+	/**
+	 *
+	 * @return Illuminate\Auth\UserInterface
+	 */
+	public function createModel()
+	{
+		$model = '\\' . ltrim($this->model, '\\');
 
-        return $grps;
-    }
+		return new $model;
+	}
 
-    public function getModel()
-    {
-        return $this->model;
-    }
+	/**
+	 * Add Ldap fields to current user model.
+	 *
+	 * @param Illuminate\Auth\UserInterface $model
+	 * @param adLDAP\collection\adLDAPCollection $ldap
+	 * @return Illuminate\Auth\UserInterface
+	 */
+	protected function addLdapToModel($model, $ldap)
+	{
+		$combined = $ldap + $model->getAttributes();
 
-    protected function getUsernameField()
-    {
-        return isset($this->config['username_field'])?$this->config['username_field']:'username';
-    }
+		return $model->fill($combined);
+	}
+
+	/**
+	 * Return Primary Group Listing
+	 * @param  array $groupList
+	 * @return string
+	 */
+	protected function getPrimaryGroup($groupList)
+	{
+		$groups = explode(',', $groupList);
+
+		return substr($groups[1], '3');
+	}
+
+	/**
+	 * Return list of groups (except domain and suffix)
+	 * @param  array $groups
+	 * @return array
+	 */
+	protected function getAllGroups($groups)
+	{
+		$grps = '';
+		if ( ! is_null($groups) ) {
+			if (!is_array($groups)) {
+				$groups = explode(',', $groups);
+			}
+			foreach ($groups as $k => $group) {
+				$splitGroups = explode(',', $group);
+				foreach ($splitGroups as $splitGroup) {
+					if (substr($splitGroup,0, 3) !== 'DC=') {
+						$grps[substr($splitGroup, '3')] = substr($splitGroup, '3');
+					}
+				}
+			}
+		}
+
+		return $grps;
+	}
+
+	public function getModel()
+	{
+		return $this->model;
+	}
+
+	protected function getUsernameField()
+	{
+		return isset($this->config['username_field'])?$this->config['username_field']:'username';
+	}
 }
